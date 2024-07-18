629f5da46e8f5f2dfcec4ce282e37a5c64d625f8
lee <lee@chris-macbook.local>
2024-07-18 13:43:46 +0800

    20240718

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            imageSrc: 'img/firstpage.png',
+            isSmallScreen: false
+        };
+    },
+    mounted() {
+        this.updateImage();
+        this.checkScreenSize();
+        window.addEventListener('resize', this.updateImage);
+        window.addEventListener('resize', this.checkScreenSize);
+    },
+    beforeUnmount() {
+        window.removeEventListener('resize', this.updateImage);
+        window.removeEventListener('resize', this.checkScreenSize);
+    },
+    methods: {
+        updateImage() {
+            if (window.innerWidth >= 1180 && window.innerWidth <= 1024) {
+                this.imageSrc = 'img/firstpage_ipad.png';
+            } else {
+                this.imageSrc = 'img/firstpage.png';
+            }
+        },
+        checkScreenSize() {
+            this.isSmallScreen = window.innerWidth < 1180;
+        }
+    }
+}).mount('#app');
